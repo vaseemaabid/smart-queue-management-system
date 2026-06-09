d12aabbac966f9d75f703bab2494502f2d894207
vaseemaabid <154531298+vaseemaabid@users.noreply.github.com>
2026-06-09 15:13:07 +0530

    Implement JWT authentication and protected routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
+
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "smartqueue_secret_key";
 
 const app = express();
 
@@ -8,6 +12,68 @@ app.use(express.json());
 
 let queue = [];
 let tokenNumber = 1;
+
+/* JWT Verification Middleware */
+
+function verifyToken(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(403).json({
+            message: "Token required"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                message: "Invalid token"
+            });
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
+
+/* Login API */
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (
+        email === "admin@smartqueue.com" &&
+        password === "admin123"
+    ) {
+
+        const token = jwt.sign(
+            {
+                email,
+                role: "admin"
+            },
+            SECRET_KEY,
+            {
+                expiresIn: "1h"
+            }
+        );
+
+        return res.json({
+            message: "Login successful",
+            token
+        });
+    }
+
+    res.status(401).json({
+        message: "Invalid credentials"
+    });
+});
+
+/* Book Token */
 
 app.post("/book-token", (req, res) => {
 
@@ -24,9 +90,13 @@ app.post("/book-token", (req, res) => {
     res.json(token);
 });
 
-app.get("/queue", (req, res) => {
+/* Protected Queue API */
+
+app.get("/queue", verifyToken, (req, res) => {
     res.json(queue);
 });
+
+/* Call Next Token */
 
 app.put("/next-token", (req, res) => {
 
@@ -42,3 +112,4 @@ app.put("/next-token", (req, res) => {
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
+
